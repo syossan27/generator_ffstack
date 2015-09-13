906e03759cbfc1855c88187037e4b059959e352d
syossan27 <wisdom1027@gmail.com>
2015-09-13 22:05:29 +0900

    change RoutingStructure & move MithrilJS file

---
+++ client/app/account/signup.js
@@ -0,0 +1,24 @@
+// Components
+var signup = {};
+
+// View-Model
+signup.vm = (function() {
+  var vm = {}
+  vm.init = function() {
+  }
+
+  return vm
+}())
+
+// Controller
+signup.controller = function() {
+    signup.vm.init()
+}
+
+//here's the view
+signup.view = function() {
+  return [
+         ];
+};
+
+module.exports = signup
---
+++ client/app/router.js
@@ -0,0 +1,6 @@
+var main = require("./main/main")
+var signup = require(".//account/signup")
+m.route(document.getElementById("root"), "/", {
+    "/": main,
+    "/signup": signup,
+});
